023f8fcce17899d4d231226f5775916977d15f46
Nicolas Märchy <nm@studer-raimann.ch>
2018-06-22 09:31:05 +0200

    Add missing ts doc

--- src/api/search/search.api.ts
+++ src/api/search/search.api.ts
@@ -6,8 +6,30 @@
  */
 export interface DocumentSearch {
 
+    /**
+     * Searches the given {@code query} by considering the given {@code options}.
+     *
+     * Depending on the {@code options} the results are highlighted in the DOM.
+     *
+     * @param {string} query - the query to search for
+     * @param {SearchOptions} options - the search options to apply
+     */
     search(query: string, options: SearchOptions): void;
+
+    /**
+     * Searches the next search result found by the {@link DocumentSearch#search} method.
+     *
+     * Depending on the {@code options} passed in to the {@code DocumentSearch#search} method
+     * the results are highlighted in the DOM.
+     */
     next(): void;
+
+    /**
+     * Searches the previous search result found by the {@link DocumentSearch#search} method.
+     *
+     * Depending on the {@code options} passed in to the {@code DocumentSearch#search} method
+     * the results are highlighted in the DOM.
+     */
     previous(): void;
 }
 
