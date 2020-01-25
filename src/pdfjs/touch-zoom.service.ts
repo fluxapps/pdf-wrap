11c967c806281eb8ab64b9cab22315c07dff803f
Nicolas Schäfli <ns@studer-raimann.ch>
2020-01-26 00:32:02 +0100

    Cleanup pinch zoom event handler on document close

--- src/pdfjs/touch-zoom.service.ts
+++ src/pdfjs/touch-zoom.service.ts
@@ -18,6 +18,8 @@ export class DefaultTouchZoomService implements TouchZoomService {
     private initialPinchDistance: number = 0;
     private pinchScale: number = 1;
 
+    private unsubscribe: (() => void) | null = null;
+
     get pinchZoomEnabled(): boolean {
         return this.isPinchZoomEnabled;
     }
@@ -26,7 +28,10 @@ export class DefaultTouchZoomService implements TouchZoomService {
         this.isPinchZoomEnabled = value;
         if (value) {
             this.enablePinchZoom();
+            return;
         }
+
+        this.disablePinchZoom();
     }
 
 
@@ -42,19 +47,9 @@ export class DefaultTouchZoomService implements TouchZoomService {
             return;
         }
 
-        // Prevent native iOS page zoom
-        document.addEventListener("touchmove", (e) => { if (e.scale !== 1) { e.preventDefault(); } }, { passive: false });
-        viewer.addEventListener("touchstart", (e) => {
-            if (e.touches.length > 1) {
-                this.startX = (e.touches[0].pageX + e.touches[1].pageX) / 2;
-                this.startY = (e.touches[0].pageY + e.touches[1].pageY) / 2;
-                this.initialPinchDistance = Math.hypot((e.touches[1].pageX - e.touches[0].pageX), (e.touches[1].pageY - e.touches[0].pageY));
-            } else {
-                this.initialPinchDistance = 0;
-            }
-        });
-
-        viewer.addEventListener("touchmove", (e) => {
+        // Block ios page zoom
+        const touchMoveDocumentHandler: (e: TouchEvent) => void = (e: TouchEvent): void => { if (e.scale !== 1) { e.preventDefault(); } };
+        const touchMoveViewerHandler: (e: TouchEvent) => void = (e: TouchEvent): void => {
             if (this.initialPinchDistance <= 0 || e.touches.length < 2) { return; }
 
             const pinchDistance: number = Math.hypot((e.touches[1].pageX - e.touches[0].pageX), (e.touches[1].pageY - e.touches[0].pageY));
@@ -64,9 +59,17 @@ export class DefaultTouchZoomService implements TouchZoomService {
 
             viewer.style.transform = `scale(${this.pinchScale})`;
             viewer.style.transformOrigin = `${originX}px ${originY}px`;
-        });
-
-        viewer.addEventListener("touchend", (_) => {
+        };
+        const touchStartViewerHandler: (e: TouchEvent) => void = (e: TouchEvent): void => {
+            if (e.touches.length > 1) {
+                this.startX = (e.touches[0].pageX + e.touches[1].pageX) / 2;
+                this.startY = (e.touches[0].pageY + e.touches[1].pageY) / 2;
+                this.initialPinchDistance = Math.hypot((e.touches[1].pageX - e.touches[0].pageX), (e.touches[1].pageY - e.touches[0].pageY));
+            } else {
+                this.initialPinchDistance = 0;
+            }
+        };
+        const touchEndViewerHandler: (e: TouchEvent) => void = (_: TouchEvent): void => {
             if (this.initialPinchDistance <= 0) { return; }
 
             viewer.style.transform = `none`;
@@ -80,7 +83,27 @@ export class DefaultTouchZoomService implements TouchZoomService {
             container.scrollTop += dy * (this.pinchScale - 1);
 
             this.reset();
-        });
+        };
+
+        // Prevent native iOS page zoom
+        document.addEventListener("touchmove", touchMoveDocumentHandler, { passive: false });
+        viewer.addEventListener("touchstart", touchStartViewerHandler);
+        viewer.addEventListener("touchmove", touchMoveViewerHandler);
+        viewer.addEventListener("touchend", touchEndViewerHandler);
+
+        this.unsubscribe = (): void => {
+            document.removeEventListener("touchmove", touchMoveDocumentHandler);
+            viewer.removeEventListener("touchstart", touchStartViewerHandler);
+            viewer.removeEventListener("touchmove", touchMoveViewerHandler);
+            viewer.removeEventListener("touchend", touchEndViewerHandler);
+            this.unsubscribe = null;
+        };
+    }
+
+    private disablePinchZoom(): void {
+        if (typeof this.unsubscribe === "function") {
+            this.unsubscribe();
+        }
     }
 
     private reset(): void {
